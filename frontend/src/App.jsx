97062adb698e29cdbd2d1583b448bc075d21dcf6
Shruti Shahi <yoshruti18@gmail.com>
2025-04-04 02:00:05 +0530

    corrections

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,7 +30,7 @@ function App() {
       <ApolloProvider client={apolloClient}>
         <Router>
           <Routes>
-            <Route path="/" element={<Login />} />
+            <Route path="/" element={<Signup />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/dashboard" element={<Dashboard />} />
